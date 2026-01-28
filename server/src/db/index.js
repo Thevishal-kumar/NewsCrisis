6a0b6956a35644b028d4f9f6a47eb372db88d1c5
vishal <kumarvishalsingh966@hmail.com>
2026-01-29 04:34:13 +0530

    Optimize MongoDB connection logic for production

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,19 +1,24 @@
-import mongoose from 'mongoose'
-import { DB_NAME } from '../constants.js'
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
 
-
-console.log('MongoDB URL:', process.env.MONGODB_URL);
-console.log('Database Name:', DB_NAME);
-const connectDB = async ()=>{
+const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        // console.log(connectionInstance)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        let mongoURL = process.env.MONGODB_URL;
+
+        if (!mongoURL) {
+            console.error("MONGODB_URL is missing in environment variables!");
+            process.exit(1);
+        }
+        mongoURL = mongoURL.replace(/\/$/, "");
+
+        console.log(`⏳ Connecting to Database: ${DB_NAME}...`);
+        const connectionInstance = await mongoose.connect(`${mongoURL}/${DB_NAME}`);
+        console.log(`\nMongoDB Connected! DB HOST: ${connectionInstance.connection.host}`);
         
     } catch (error) {
-        console.log("MONGODB connection error",error);
-        process.exit(1)
+        console.error("MONGODB connection error:", error.message);
+        process.exit(1);
     }
-} 
+}
 
-export default connectDB
+export default connectDB;
