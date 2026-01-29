631c6f64a412a69ab5273ae3593169f8f876b4b9
vishal <kumarvishalsingh966@hmail.com>
2026-01-29 15:29:38 +0530

    fix port in ml

--- ml/ml_server.py
+++ ml/ml_server.py
@@ -51,4 +51,5 @@ def predict():
 
 if __name__ == '__main__':
     # Runs on localhost port 5000
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
